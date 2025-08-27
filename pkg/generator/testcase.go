941329da0a3d7515f22263b5794030953f15ca69
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-27 08:33:37 -0400

    spelling: also
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/generator/testcase.go
+++ pkg/generator/testcase.go
@@ -1,12 +1,13 @@
 package generator
 
 import (
+	"sort"
+	"strings"
+
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"sort"
-	"strings"
 )
 
 type TestCase struct {
@@ -56,7 +57,7 @@ func (t *TestCase) collectActionsAndPolicies() (map[string]bool, []*networkingv1
 			} else if action.DeleteNamespace != nil {
 				features[ActionFeatureDeleteNamespace] = true
 			} else if action.ReadNetworkPolicies != nil {
-				// TODO need to also analyze these policies after they get read
+				// TODO also analyze these policies after they get read
 				features[ActionFeatureReadPolicies] = true
 			} else if action.CreatePod != nil {
 				features[ActionFeatureCreatePod] = true
